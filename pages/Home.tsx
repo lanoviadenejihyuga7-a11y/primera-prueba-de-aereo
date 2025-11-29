2442bf3a8fe4d270518799e5fc8d2a0128e1bb33
lanoviadenejihyuga7-a11y <lanoviadenejihyuga7@gmail.com>
2025-11-28 21:40:13 -0500

    Update Home.tsx

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -700,7 +700,7 @@ const Home: React.FC = () => {
                                 >
                                     {registerStatus === 'loading' ? (
                                         <>
-                                            <svg className="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                            <svg className="animate-spin h-5 w-5 text-white" xmlns="https://docs.google.com/spreadsheets/d/1zQkZAvXhgSeukJP2opHR9rEnCBZ-zQdlq7IHxWVy3E8/edit?usp=sharing" fill="none" viewBox="0 0 24 24">
                                                 <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                                                 <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                                             </svg>
